ae6480356fd0301bfb183f865dbf117d369d2db6
FelipeFTN <felipe.tenorio123456@gmail.com>
2021-08-02 17:32:21 -0300

    Games

--- Games/HorrorGame/Main.py
+++ Games/HorrorGame/Main.py
@@ -1,23 +1,13 @@
+# Imports
 import pygame
 
-# Initialize PyGame
-pygame.init()
-
-# Create the window
+# Initialize pygame
 screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Dark Legend")
-icon = pygame.image.load("images/Logo.png")
-pygame.display.set_icon(icon)
 
-# Player
-playerImg = pygame.image.load('images/PlayerResized.png')
-playerX = 370
-playerY = 480
-playerDirectionX = 0
-playerDirectionY = 0
-
-def player(x, y):
-	screen.blit(playerImg, (x, y))
+# Caption and Icon
+pygame.display.set_caption("Horror Game")
+#icon = pygame.image.load('Horror.png')
+#pygame.display.set_icon(icon)
 
 # Game Loop
 running = True
@@ -25,25 +15,3 @@ while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LEFT:
-				playerDirectionX = -0.05
-			if event.key == pygame.K_RIGHT:
-				playerDirectionX = 0.05
-			if event.key == pygame.K_UP:
-				playerDirectionY = -0.05
-			if event.key == pygame.K_DOWN:
-				playerDirectionY = 0.05
-
-		if event.type == pygame.KEYUP:
-			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT: 
-				playerDirectionX = 0
-			if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-				playerDirectionY = 0
-
-	screen.fill((0, 0, 0))
-
-	playerX += playerDirectionX
-	playerY += playerDirectionY
-	player(playerX, playerY)
-	pygame.display.update()

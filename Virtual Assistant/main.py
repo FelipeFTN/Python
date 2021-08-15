c2908502b4bdf982d27db6bf74e6ec818d8ecda5
FelipeFTN <felipe.tenorio123456@gmail.com>
2021-08-15 20:44:00 -0300

    Improving AI

--- Virtual Assistant/main.py
+++ Virtual Assistant/main.py
@@ -6,22 +6,31 @@ listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
-engine.say('Sistema Ligado')
-engine.runAndWait()
 
 def talk(textotalk):
 	engine.say(textotalk)
 	engine.runAndWait()
 
-try:
-	with sr.Microphone() as source:
-		listener.adjust_for_ambient_noise(source)
-		print('Say something!')
-		audio = listener.listen(source)
-		command = listener.recognize_google(audio, language='pt-br')
-		command = command.lower()
-		if 'bom dia' in command:
-			talk('Bom Dia Felipe')
-		print(command)
-except:
-	print("Say it again please!")
+def listen():
+	try:
+		with sr.Microphone() as source:
+			listener.adjust_for_ambient_noise(source)
+			print('Escutando...')
+			audio = listener.listen(source)
+			command = listener.recognize_google(audio, language='pt-br')
+			command = command.lower()
+			if 'computador' in command:
+				command = command.replace('computador', '')
+	except:
+		pass
+	return command
+
+def run():
+	command = listen()
+	if 'tocar' in command:
+		song = command.replace('tocar', '')
+		talk('Reproduzindo ' + song)
+		pywhatkit.playonyt(song)
+
+while True:
+	run()
